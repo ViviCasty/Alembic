f5abeca82ee327faeba48405e88f42d389a13b91
ViviCasty <ichpendragon@gmail.com>
2025-09-24 01:31:57 -0300

    Crição de modelos e testando as migraçoes

---
+++ migrations/models.py
@@ -0,0 +1,23 @@
+from sqlalchemy import Integer, String
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
+
+class Base(DeclarativeBase):
+    pass
+
+
+class Pessoa(Base):
+    __tablename__ = 'pessoa'
+
+    id: Mapped[int] = mapped_column(Integer, primary_key=True)
+    nome: Mapped[str] = mapped_column(String(50), nullable=False)
+    email: Mapped[str] = mapped_column(String(50), nullable=False)
+    senha: Mapped[str] = mapped_column(String(50), nullable=False)
+
+class Pessoa2(Base):
+    __tablename__ = 'pessoa2'
+
+    id: Mapped[int] = mapped_column(Integer, primary_key=True)
+    nome: Mapped[str] = mapped_column(String(50), nullable=False)
+    email: Mapped[str] = mapped_column(String(50), nullable=False)
+    senha: Mapped[str] = mapped_column(String(50), nullable=False)
+    idade: Mapped[int] = mapped_column(Integer, nullable=False)
